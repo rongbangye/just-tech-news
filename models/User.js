c2bd369acfdff9bb5d0d9a51e269c8390eb845d9
rongbang <rongbang.ye@gmail.com>
2020-10-14 16:03:42 -0700

    added security features, hashed user's password to keep an encrypted version in the db

--- models/User.js
+++ models/User.js
@@ -1,8 +1,17 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    // Using the keyword this,
+    // we can access this user's properties, including the password,
+    // which was stored as a hashed string.
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -44,6 +53,22 @@ User.init(
     },
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      // use async await instead of Promise
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -64,6 +64,30 @@ router.post("/", (req, res) => {
     });
 });
 
+// Login Post Request
+router.post("/login", (req, res) => {
+  // Query operation
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
+});
+
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -75,6 +99,7 @@ router.put("/:id", (req, res) => {
    * WHERE id = 1;
    */
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
